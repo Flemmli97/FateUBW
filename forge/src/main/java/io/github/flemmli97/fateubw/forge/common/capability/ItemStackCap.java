aa77adbd1db7d9f45a2751cec6fcf69efb103850
Flemmli97 <Flemmli97@users.noreply.github.com>
2024-03-04 16:07:36 +0100

    update to loom 1.4

--- forge/src/main/java/io/github/flemmli97/fateubw/forge/common/capability/ItemStackCap.java
+++ forge/src/main/java/io/github/flemmli97/fateubw/forge/common/capability/ItemStackCap.java
@@ -5,17 +5,16 @@ import net.minecraft.core.Direction;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.common.capabilities.ICapabilityProvider;
 import net.minecraftforge.common.util.LazyOptional;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class ItemStackCap extends ItemStackData implements ICapabilityProvider {
 
     private final LazyOptional<ItemStackData> instance = LazyOptional.of(() -> this);
 
-    @Nonnull
+    @NotNull
     @Override
-    public <T> LazyOptional<T> getCapability(@Nonnull Capability<T> cap, @Nullable Direction side) {
+    public <T> LazyOptional<T> getCapability(@NotNull Capability<T> cap, @Nullable Direction side) {
         return CapabilityInsts.ITEMSTACKCAP.orEmpty(cap, this.instance);
     }
 }
