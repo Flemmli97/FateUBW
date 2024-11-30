2937c50808aa6c313ef5802e8b41011d9e45955e
Flemmli97 <Flemmli97@users.noreply.github.com>
2024-11-30 18:11:09 +0100

    hooks for archers bow

--- common/src/main/java/io/github/flemmli97/fateubw/common/items/weapons/ItemArcherBow.java
+++ common/src/main/java/io/github/flemmli97/fateubw/common/items/weapons/ItemArcherBow.java
@@ -17,6 +17,7 @@ import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.projectile.AbstractArrow;
 import net.minecraft.world.item.BowItem;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
@@ -104,7 +105,7 @@ public class ItemArcherBow extends BowItem implements SwingItem {
                 float f = getPowerForTime(i * 2);
                 if (f >= 0.1D) {
                     if (!world.isClientSide) {
-                        ArcherArrow arrow = new ArcherArrow(player.level, player);
+                        AbstractArrow arrow = this.customArrow(new ArcherArrow(player.level, player)); // Forge
                         arrow.shootFromRotation(player, player.getXRot(), player.getYRot(), 0.0F, f * 2.5F, 1.0F);
                         if (f == 1.0F)
                             arrow.setCritArrow(true);
@@ -128,6 +129,10 @@ public class ItemArcherBow extends BowItem implements SwingItem {
                 }
             }
         }
+    }
+
+    public AbstractArrow customArrow(AbstractArrow arrow) {
+        return new ArcherArrow(arrow.level, arrow.getOwner() instanceof LivingEntity owner ? owner : null);
     }
 
     public boolean charged(ItemStack stack) {
