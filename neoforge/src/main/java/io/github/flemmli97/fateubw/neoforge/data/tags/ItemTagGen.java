aceaaab3cd9d01a9a61d44e3677e4e7382649b4d
Flemmli97 <Flemmli97@users.noreply.github.com>
2025-09-24 13:54:38 +0200

    tags

--- neoforge/src/main/java/io/github/flemmli97/fateubw/neoforge/data/tags/ItemTagGen.java
+++ neoforge/src/main/java/io/github/flemmli97/fateubw/neoforge/data/tags/ItemTagGen.java
@@ -8,6 +8,7 @@ import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.world.level.block.Block;
+import net.neoforged.neoforge.common.Tags;
 
 import java.util.concurrent.CompletableFuture;
 
@@ -32,9 +33,34 @@ public class ItemTagGen extends ItemTagsProvider {
                 .add(FateItems.GAEDEARG.get());
         this.tag(FateTags.Items.SPEARS)
                 .addTag(FateTags.Items.MOD_SPEARS);
-        this.tag(ItemTags.SWORD_ENCHANTABLE)
+        this.tag(ItemTags.FIRE_ASPECT_ENCHANTABLE)
+                .addTag(FateTags.Items.MOD_SPEARS);
+        this.tag(ItemTags.SHARP_WEAPON_ENCHANTABLE)
                 .addTag(FateTags.Items.MOD_SPEARS);
         this.tag(ItemTags.TRIDENT_ENCHANTABLE)
                 .addTag(FateTags.Items.MOD_SPEARS);
+
+        this.tag(ItemTags.SWORDS)
+                .add(FateItems.INVISEXCALIBUR.get())
+                .add(FateItems.EXCALIBUR.get())
+                .add(FateItems.KANSHOU.get())
+                .add(FateItems.BAKUYA.get())
+                .add(FateItems.BAKUYA.get())
+                .add(FateItems.ENUMAELISH.get())
+                .add(FateItems.RULE_BREAKER.get())
+                .add(FateItems.ARONDIGHT.get())
+                .add(FateItems.KUPRIOTS.get())
+                .add(FateItems.MEDUSA_DAGGER.get())
+                .add(FateItems.ASSASSIN_DAGGER.get())
+                .add(FateItems.MONOHOSHI_ZAO.get());
+        this.tag(ItemTags.AXES)
+                .add(FateItems.HERACLES_AXE.get());
+        this.tag(Tags.Items.TOOLS_BOW)
+                .add(FateItems.EMIYAS_BOW.get());
+        this.tag(ItemTags.BOW_ENCHANTABLE)
+                .add(FateItems.EMIYAS_BOW.get());
+        this.tag(ItemTags.DURABILITY_ENCHANTABLE)
+                .addTag(FateTags.Items.MOD_SPEARS)
+                .add(FateItems.EMIYAS_BOW.get());
     }
 }
